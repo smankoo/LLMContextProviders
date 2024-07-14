05780d19e86fe261342580690d2d73ca0069918e
Sumeet Singh Mankoo <sumeet@mankoo.ca>
2024-07-14 16:20:52 -0400

    push to pypi on ubuntu only

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8'
 
 setup(
     name='LLMContextProviders',
-    version='0.1.7',
+    version='0.1.8',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
