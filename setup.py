c91205da4675eac771efd6d5ff0f664e2c217d20
Sumeet Singh Mankoo <sumeet@mankoo.ca>
2024-07-14 17:16:26 -0400

    bumped pypi version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8'
 
 setup(
     name='llm-context-providers',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
